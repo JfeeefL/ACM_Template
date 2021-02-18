361ef27fd369bd6f74bb335f34ab50ea39057708
Jeff Lee <1531488917@qq.com>
2021-02-18 23:27:10 +0800

    Add files via upload

---
+++ BIT.cpp
@@ -0,0 +1,53 @@
+﻿#include <vector>
+
+typedef unsigned long long int size_type;
+
+template <typename T> class BIT_BASE {
+	private: 
+		std::vector<T> s;
+		inline size_type lowbit(size_type t) {
+			return t&(-t);
+		}
+	protected: 
+	virtual T query(size_type p) {
+		T ans = 0;
+		while(p < s.size()) {
+			ans += s[p];
+			p += lowbit(p);
+		}
+		return ans;
+	}
+	virtual void add(size_type p, T a) {
+		while(p) {
+			s[p] += a;;
+			p ^= lowbit(p);
+		}
+	}
+	public:
+		BIT_BASE () {}
+		void resize(size_type n) {
+			s.resize(n+1,0);
+		}
+		BIT_BASE(size_type n): s(n) {
+			resize(n);
+		}
+};
+
+template<typename T> class BIT: public BIT_BASE<T> {
+	public:
+	BIT() {}
+	BIT(size_type size) {
+		BIT_BASE<T>::resize(size);
+	}
+	T query(size_type position) {
+		return BIT_BASE<T>::query(position);
+	}
+	void modify(size_type position, T value) {
+		BIT_BASE<T>::add(position, value);
+	}
+};
+
+int main() {
+	BIT<int> t(10);
+	return 0;
+}
---
+++ read_data.cpp
@@ -0,0 +1,58 @@
+﻿#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+template<typename T> class input {
+	protected:
+		vector<T> data;
+		function<T(void)> each_element;
+		void read() {
+			scanf("%d", &size);
+			data.resize(size);
+			for(int i = 0; i < size; i ++) {
+				data[i] = each_element();
+			}
+		}
+	public:
+		int size;
+		input() {}
+		input(function<T(void)> f): each_element(f) {
+			read();
+		}
+		T& operator [] (int id) {
+			return data[id];
+		}
+		void sort() {
+			std::sort(data.begin(),data.end());
+		}
+		void sort(function<bool(T,T)> f) {
+			std::sort(data.begin(),data.end(),f);
+		}
+};
+
+inline int single_int() {
+	int x;
+	scanf("%d", &x);
+	return x;
+}
+
+inline pair<int,int> pair_int() {
+	pair<int,int> x;
+	scanf("%d%d", &x.first, &x.second);
+	return x;
+}
+
+int main() {
+	input<int> a(single_int);
+//	input<pair<int,int> > q(pair_int);
+	a.sort([](int a,int b)->bool{return a>b;});
+	for(int i = 0; i < a.size; i ++) {
+		printf("%d\n", a[i]);
+	}
+	//
+//	q.sort();
+	return 0;
+}
